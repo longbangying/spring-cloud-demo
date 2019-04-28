ae1725b20888757056efb0102311875fbd63b723
1523550440@qq.com <1523550440@qq.com>
2019-04-28 21:30:28 +0800

    --zuul

---
+++ spring-cloud-demo/spring-cloud-zuul-demo/src/main/java/org/spring/cloud/zuul/demo/ZuulApplication.java
@@ -0,0 +1,20 @@
+package org.spring.cloud.zuul.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication 
+{
+    public static void main( String[] args )
+    {
+       SpringApplication.run(ZuulApplication.class, args);
+       System.out.println("Zuul服务启动成功.");
+    }
+}
